6c5ce5f09be16836544d355cb86c102315731049
jakob83 <jakobpllo@gmail.com>
2024-08-09 15:31:04 +0200

    initial

--- src/writeError.js
+++
@@ -1,9 +0,0 @@
-const errorP = document.querySelector('.error-msg');
-
-function removeError() {
-    errorP.innerText = '';
-}
-function writeError(error) {
-    errorP.innerText = error;
-}
-export { removeError, writeError };
